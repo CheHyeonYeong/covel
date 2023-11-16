b0e32d66a35c64817d524ee90b7a1575f0b7110f
CheHyeonyeong <gusdudco6@naver.com>
2023-11-16 22:16:48 +0900

    page fix

---
+++ front/webtoon/js/nav.js
@@ -0,0 +1,67 @@
+
+const navbaridea =
+`
+<!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container px-4 px-lg-5">
+                
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="./body.html">Home</a></li>
+                        
+                        <li class="nav-item"><a class="nav-link" 
+                            href="./mypage.html">mypage</a></li>
+                        <li class="nav-item"><a class="nav-link" 
+                            href="./search.html">search</a></li>
+                       
+                    </ul>
+                
+                    <form class="d-flex">
+                        <!--btn -> 인간 동글뱅이로 바꿔죠....-->
+                        <a class="navbar-brand" href="./mypage/login.html">Login</a>
+                        <a class="navbar-brand" href="./mypage/register.html">회원가입</a>
+
+                    </form>
+                </div>
+            </div>
+        </nav>
+`;
+
+
+/*<!-- Navigation-->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<div class="container px-4 px-lg-5">
+    
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
+            <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">Home</a></li>
+            
+            <li class="nav-item"><a class="nav-link" 
+                href=".\mypage\index.html">mypage</a></li>
+            <li class="nav-item"><a class="nav-link" 
+                href=".\search.html">search</a></li>
+            <!--<li class="nav-item dropdown">
+                 nav toggle
+                <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">WebtoonShop</a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <li><a class="dropdown-item" href="#!">All Products</a></li>
+                    <li><hr class="dropdown-divider" /></li>
+                    <li><a class="dropdown-item" href="#!">Popular Items</a></li>
+                    <li><a class="dropdown-item" href="#!">New Arrivals</a></li>
+                </ul>
+            </li>-->
+        </ul>
+    
+        <form class="d-flex">
+            <!--btn -> 인간 동글뱅이로 바꿔죠....-->
+            <a class="navbar-brand" href="./mypage/login.html">Login</a>
+            <a class="navbar-brand" href="./mypage/register.html">회원가입</a>
+
+        </form>
+    </div>
+</div>
+</nav>*/
